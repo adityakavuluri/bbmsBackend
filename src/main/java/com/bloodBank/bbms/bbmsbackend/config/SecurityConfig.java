daf6bc940a075519c879cbbd714ef025e1944587
Aditya <aditya@Adityas-MacBook-Pro.local>
2023-11-29 14:47:51 -0500

    Errors resolved

--- src/main/java/com/bloodBank/bbms/bbmsbackend/config/SecurityConfig.java
+++ src/main/java/com/bloodBank/bbms/bbmsbackend/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.bloodBank.bbms.bbmsbackend.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,22 +11,27 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    /*@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/test").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/api/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+public class SecurityConfig{
 
-        return http.build();
-    }*/
+/*
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll() // Permit all requests without authentication
+                .and()
+                .csrf().disable(); // Disable CSRF protection if it's not needed
+    }
+*/
+@Bean
+public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .authorizeRequests()
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll() // Apply this configuration to Actuator endpoints
+            .and()
+            .csrf().disable(); // Optionally disable CSRF
+
+    return http.build();
+}
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
