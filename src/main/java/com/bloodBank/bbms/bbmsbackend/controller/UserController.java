19cd2fba387bae7866a1653c087c56498a3b7692
varshasingh13 <varshas@vt.edu>
2023-12-04 22:07:34 -0500

    all apis commit

--- src/main/java/com/bloodBank/bbms/bbmsbackend/controller/UserController.java
+++ src/main/java/com/bloodBank/bbms/bbmsbackend/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.bloodBank.bbms.bbmsbackend.controller;
 
 import com.bloodBank.bbms.bbmsbackend.LoginResponse;
+import com.bloodBank.bbms.bbmsbackend.dto.AppointmentDto;
+import com.bloodBank.bbms.bbmsbackend.dto.BloodAvailabilityDto;
 import com.bloodBank.bbms.bbmsbackend.dto.LoginDto;
 import com.bloodBank.bbms.bbmsbackend.dto.SignUpDto;
+import com.bloodBank.bbms.bbmsbackend.entity.Appointment;
+import com.bloodBank.bbms.bbmsbackend.entity.BloodBankData;
 import com.bloodBank.bbms.bbmsbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @CrossOrigin
@@ -29,9 +35,65 @@ private UserService userService;
         return ResponseEntity.ok(loginResponse);
     }
 
+    @PostMapping(path = "/bookAppointment")
+    public String bookDonorAppointment(@RequestBody AppointmentDto appointmentDto)
+    {
+        String id = userService.newAppointment(appointmentDto);
+        return id;
+    }
+
+    @GetMapping("/upcomingAppointments")
+    public ResponseEntity<List<Appointment>> getUpcomingAppointments() {
+        List<Appointment> upcomingAppointments = userService.getUpcomingAppointmentsForUser();
+        return ResponseEntity.ok(upcomingAppointments);
+    }
+
+    @GetMapping("/getReceipientData")
+    public ResponseEntity<List<Appointment>> getReceipientData() {
+        List<Appointment> receipientData = userService.getReceipientData();
+        return ResponseEntity.ok(receipientData);
+    }
+
+    @GetMapping("/getDonorData")
+    public ResponseEntity<List<Appointment>> getDonorData() {
+        List<Appointment> donorData = userService.getDonorData();
+        return ResponseEntity.ok(donorData);
+    }
+
+    @GetMapping("/adminNewAppointments")
+    public ResponseEntity<List<Appointment>> getAdminNewAppointments() {
+        List<Appointment> adminNewAppointments = userService.getAdminNewAppointments();
+        return ResponseEntity.ok(adminNewAppointments);
+    }
+
+    @GetMapping("/adminPastAppointments")
+    public ResponseEntity<List<Appointment>> getAdminPastAppointments() {
+        List<Appointment> adminPastAppointments = userService.getAdminPastAppointments();
+        return ResponseEntity.ok(adminPastAppointments);
+    }
+
+    @GetMapping("/getBloodBankdata")
+    public ResponseEntity<List<BloodBankData>> getBloodBankdata() {
+        List<BloodBankData> bloodBankData = userService.getBloodBankdata();
+        return ResponseEntity.ok(bloodBankData);
+    }
+
+
+
+
     @GetMapping(path = "/test")
     public String demo(){
 
         return "Hi";
     }
+
+    @GetMapping("/bloodAvailability")
+    public String getBloodAvailability(@RequestParam String bloodType, @RequestParam String quantity) {
+         return userService.checkBloodAvailability(bloodType, quantity);
+
+    }
+
+    //bloodAvailability
+
+
 }
--- src/main/java/com/bloodBank/bbms/bbmsbackend/dto/SignUpDto.java
+++ src/main/java/com/bloodBank/bbms/bbmsbackend/dto/SignUpDto.java
@@ -29,4 +29,20 @@ public class SignUpDto {
     private Integer age;
 
     private String sex;
+
+    public SignUpDto(Integer id, String email, String password, String firstName, String lastName, String address, String country, Integer pincode, String phone, String medicalHistory, String bloodGroup, Integer age, String sex) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.country = country;
+        this.pincode = pincode;
+        this.phone = phone;
+        this.medicalHistory = medicalHistory;
+        this.bloodGroup = bloodGroup;
+        this.age = age;
+        this.sex = sex;
+    }
 }
